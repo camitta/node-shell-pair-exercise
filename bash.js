c1b5e5ce40844fc7d220b205328e8c28e7f6c5e9
Desiree Nelson <palebluedot4@gmail.com>
2020-11-13 15:10:35 -0500

    added cwd command

--- bash.js
+++ bash.js
@@ -1,8 +1,13 @@
-process.stdout.write('prompt > ');
+process.stdout.write("prompt > ");
 
-process.stdin.on('data', (data) => {
-	const cmd = data.toString().trim();
+process.stdin.on("data", (data) => {
+  const cmd = data.toString().trim();
 
-	process.stdout.write('You typed: ' + cmd);
-	process.stdout.write('\nprompt > ');
+  if (cmd === "cwd") {
+    process.stdout.write(`Current directory: ${process.cwd()}`);
+  }
+
+  //process.stdout.write("You typed: " + cmd);
+
+  process.stdout.write("\nprompt > ");
 });
